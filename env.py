7000771ae337bd4be5b378a758c2dbf4466a491b
Wilson Yan <wilson1.yan@berkeley.edu>
2022-05-16 14:55:27 -0700

    sub select biome support

--- env.py
+++ env.py
@@ -1,4 +1,5 @@
 from typing import List
+import random
 from minerl.herobraine.env_specs.simple_embodiment import SIMPLE_KEYBOARD_ACTION, SimpleEmbodimentEnvSpec
 from minerl.herobraine.hero.handler import Handler
 import minerl.herobraine.hero.handlers as handlers
@@ -9,94 +10,13 @@ SIMPLE_KEYBOARD_ACTIONS = [
     'forward',
     'jump'
 ]
-TREECHOP_WORLD_GENERATOR_OPTIONS = '''{
-    "coordinateScale": 684.412,
-    "heightScale": 684.412,
-    "lowerLimitScale": 512.0,
-    "upperLimitScale": 512.0,
-    "depthNoiseScaleX": 200.0,
-    "depthNoiseScaleZ": 200.0,
-    "depthNoiseScaleExponent": 0.5,
-    "mainNoiseScaleX": 80.0,
-    "mainNoiseScaleY": 160.0,
-    "mainNoiseScaleZ": 80.0,
-    "baseSize": 8.5,
-    "stretchY": 12.0,
-    "biomeDepthWeight": 1.0,
-    "biomeDepthOffset": 0.0,
-    "biomeScaleWeight": 1.0,
-    "biomeScaleOffset": 0.0,
-    "seaLevel": 1,
-    "useCaves": false,
-    "useDungeons": false,
-    "dungeonChance": 8,
-    "useStrongholds": false,
-    "useVillages": false,
-    "useMineShafts": false,
-    "useTemples": false,
-    "useMonuments": false,
-    "useMansions": false,
-    "useRavines": false,
-    "useWaterLakes": false,
-    "waterLakeChance": 4,
-    "useLavaLakes": false,
-    "lavaLakeChance": 80,
-    "useLavaOceans": false,
-    "fixedBiome": 4,
-    "biomeSize": 4,
-    "riverSize": 1,
-    "dirtSize": 33,
-    "dirtCount": 10,
-    "dirtMinHeight": 0,
-    "dirtMaxHeight": 256,
-    "gravelSize": 33,
-    "gravelCount": 8,
-    "gravelMinHeight": 0,
-    "gravelMaxHeight": 256,
-    "graniteSize": 33,
-    "graniteCount": 10,
-    "graniteMinHeight": 0,
-    "graniteMaxHeight": 80,
-    "dioriteSize": 33,
-    "dioriteCount": 10,
-    "dioriteMinHeight": 0,
-    "dioriteMaxHeight": 80,
-    "andesiteSize": 33,
-    "andesiteCount": 10,
-    "andesiteMinHeight": 0,
-    "andesiteMaxHeight": 80,
-    "coalSize": 17,
-    "coalCount": 20,
-    "coalMinHeight": 0,
-    "coalMaxHeight": 128,
-    "ironSize": 9,
-    "ironCount": 20,
-    "ironMinHeight": 0,
-    "ironMaxHeight": 64,
-    "goldSize": 9,
-    "goldCount": 2,
-    "goldMinHeight": 0,
-    "goldMaxHeight": 32,
-    "redstoneSize": 8,
-    "redstoneCount": 8,
-    "redstoneMinHeight": 0,
-    "redstoneMaxHeight": 16,
-    "diamondSize": 8,
-    "diamondCount": 1,
-    "diamondMinHeight": 0,
-    "diamondMaxHeight": 16,
-    "lapisSize": 7,
-    "lapisCount": 1,
-    "lapisCenterHeight": 16,
-    "lapisSpread": 16
-}'''
 
 
 class SimpleExplore(SimpleEmbodimentEnvSpec):
-    def __init__(self, forest_only, *args, **kwargs):
+    def __init__(self, *args, biomes=None, **kwargs):
         if 'name' not in kwargs:
             kwargs['name'] = 'SimpleExplore-v0'
-        self.forest_only = forest_only
+        self.biomes = biomes
         super().__init__(*args, **kwargs)
 
     def create_rewardables(self) -> List[Handler]:
@@ -109,10 +29,16 @@ class SimpleExplore(SimpleEmbodimentEnvSpec):
         return []
 
     def create_server_world_generators(self) -> List[Handler]:
-        gen_options = TREECHOP_WORLD_GENERATOR_OPTIONS if self.forest_only else None
-        return [
-            handlers.DefaultWorldGenerator(force_reset="true", generator_options=gen_options)
-        ]
+        if self.biomes is not None:
+            biome = random.choice(self.biomes)
+            print('selected', biome)
+            return [
+                handlers.BiomeGenerator(biome=biome, force_reset=True)
+            ]
+        else: 
+            return [
+                handlers.DefaultWorldGenerator(force_reset=True)
+            ]
         
 
     def create_actionables(self) -> List[Handler]:
---
+++ render.py
@@ -0,0 +1,40 @@
+import numpy as np
+import gym
+from env import SimpleExplore
+
+
+ACTIONS = {
+    'forward': dict(forward=np.array(1), jump=np.array(1), camera=np.array([0., 0.])),
+    'left': dict(forward=np.array(0), jump=np.array(1), camera=np.array([0., -20.])),
+    'right': dict(forward=np.array(0), jump=np.array(1), camera=np.array([0., 20.]))
+}
+
+ACTIONS_TO_ID = {
+    'forward': 0,
+    'left': 1,
+    'right': 2
+}
+
+
+def sample_action(prob_forward):
+    prob_turn = (1 - prob_forward) / 2
+    i = np.random.choice(['forward', 'left', 'right'], 
+                         p=[prob_forward, prob_turn, prob_turn])
+    return ACTIONS[i]
+
+
+def main():
+    abs_env = SimpleExplore(resolution=(128, 128), biomes=[140, 38, 158, 133, 4, 27, 134, 8, 37, 165, 38, 166, 13, 17, 18, 19, 31])
+    abs_env.register()
+    
+    env = gym.make('SimpleExplore-v0')
+    for _ in range(100):
+        env.reset()
+        for _ in range(500):
+            env.render()
+            action = sample_action(0.9)
+            env.step(action)
+
+        
+if __name__ == '__main__':
+    main()
