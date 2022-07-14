4829da4ff1068f9805fae50056ad3f01b6d5a1b3
Wilson Yan <wilson1.yan@berkeley.edu>
2022-07-14 14:50:37 -0700

    edits

--- render.py
+++ render.py
@@ -18,23 +18,23 @@ ACTIONS_TO_ID = {
 
 def sample_action(prob_forward):
     prob_turn = (1 - prob_forward) / 2
-    i = np.random.choice(['forward', 'left', 'right'], 
+    i = np.random.choice(['forward', 'left', 'right'],
                          p=[prob_forward, prob_turn, prob_turn])
     return ACTIONS[i]
 
 
 def main():
-    abs_env = SimpleExplore(resolution=(128, 128), biomes=[140, 38, 158, 133, 4, 27, 134, 8, 37, 165, 38, 166, 13, 17, 18, 19, 31])
+    abs_env = SimpleExplore(resolution=(128, 128), biomes=[6])# biomes=[140, 38, 158, 133, 4, 27, 134, 8, 37, 165, 38, 166, 13, 17, 18, 19, 31])
     abs_env.register()
-    
+
     env = gym.make('SimpleExplore-v0')
     for _ in range(100):
         env.reset()
-        for _ in range(500):
+        for _ in range(50000000000000000000000000):
             env.render()
             action = sample_action(0.9)
             env.step(action)
 
-        
+
 if __name__ == '__main__':
     main()
